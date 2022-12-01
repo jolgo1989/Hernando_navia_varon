c3178a9e5c96b38d5863430ff81e0b78b8a91c60
jorge <jlgo60@hotmail.com>
2022-11-30 21:26:13 -0500

    Se programo los items para cerrar el menu  al dar clic en cada uno de ellos

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 const toggleMenuElement = document.getElementById("header__toggle-menu");
 const mainMenuElement = document.getElementById("header__ul");
+const menuItems = document.querySelectorAll(".header__a");
 
+
+// Metodo para cerra el menu al dar clic sobre el icono de hamburguesa
 toggleMenuElement.addEventListener('click',()=> {
     mainMenuElement.classList.toggle('header__ul--show');
 });
 
+// Metodo para cerrar el menu al dar clic en los items
+for (const menuItem of menuItems) {
+  //add click events to menu menuItems
+  menuItem.addEventListener('click',()=> {
+    //hide menu if click on menu item
+    mainMenuElement.classList.toggle('header__ul--show');
+  });
+}
