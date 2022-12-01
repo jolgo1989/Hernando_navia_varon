3a10cbc42f2c202836f0865a579c2dbd358e7655
jorge <jlgo60@hotmail.com>
2022-12-01 06:05:37 -0500

    Se agrego metodo  para ocultar el menu cuando se haga scroll

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,26 @@ const toggleMenuElement = document.getElementById("header__toggle-menu");
 const mainMenuElement = document.getElementById("header__ul");
 const menuItems = document.querySelectorAll(".header__a");
 
+//* classList.toggle (Si existe el elemento lo elimina sino lo agrega)
+//* classList.remove (Elimina el elemento)
 
-// Metodo para cerra el menu al dar clic sobre el icono de hamburguesa
+
+//Todo: Metodo para abrir y cerrar el menu al dar clic sobre el icono de hamburguesa
 toggleMenuElement.addEventListener('click',()=> {
     mainMenuElement.classList.toggle('header__ul--show');
 });
 
-// Metodo para cerrar el menu al dar clic en los items
+//Todo: Metodo para ocultar el menu al dar clic en los items
 for (const menuItem of menuItems) {
   //add click events to menu menuItems
   menuItem.addEventListener('click',()=> {
     //hide menu if click on menu item
-    mainMenuElement.classList.toggle('header__ul--show');
+    mainMenuElement.classList.remove('header__ul--show');
   });
 }
+
+//Todo: Metodo para oculatra el menu cuando usemos el scroll
+window.onscroll = () =>{
+    mainMenuElement.classList.remove('header__ul--show');
+}
+
